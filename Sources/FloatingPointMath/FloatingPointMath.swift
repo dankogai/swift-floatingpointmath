0983e3951c78d1f0636d04f34425c5a3ad3e80b2
Dan Kogai <dankogai+github@gmail.com>
2018-05-25 06:20:11 +0900

    add missing functions

--- Sources/FloatingPointMath/FloatingPointMath.swift
+++ Sources/FloatingPointMath/FloatingPointMath.swift
@@ -15,9 +15,12 @@ public protocol FloatingPointMath {
     static func cosh (_:Self)->Self
     static func exp  (_:Self)->Self
     static func log  (_:Self)->Self
+    static func log10(_:Self)->Self
     static func sin  (_:Self)->Self
     static func sinh (_:Self)->Self
     static func sqrt (_:Self)->Self
+    static func tan  (_:Self)->Self
+    static func tanh (_:Self)->Self
     static func atan2(_:Self, _:Self)->Self
     static func hypot(_:Self, _:Self)->Self
     static func pow  (_:Self, _:Self)->Self
@@ -35,9 +38,12 @@ extension Double : FloatingPointMath {
     public static func cosh (_ x:Double)->Double { return Glibc.cosh(x) }
     public static func exp  (_ x:Double)->Double { return Glibc.exp(x) }
     public static func log  (_ x:Double)->Double { return Glibc.log(x) }
+    public static func log10(_ x:Double)->Double { return Glibc.log10(x) }
     public static func sin  (_ x:Double)->Double { return Glibc.sin(x) }
     public static func sinh (_ x:Double)->Double { return Glibc.sinh(x) }
     public static func sqrt (_ x:Double)->Double { return Glibc.sqrt(x) }
+    public static func tan  (_ x:Double)->Double { return Glibc.tan(x) }
+    public static func tanh (_ x:Double)->Double { return Glibc.tanh(x) }
     public static func atan2(_ y:Double, _ x:Double)->Double { return Glibc.atan2(y, x) }
     public static func hypot(_ x:Double, _ y:Double)->Double { return Glibc.hypot(x, y) }
     public static func pow  (_ x:Double, _ y:Double)->Double { return Glibc.pow(x, y) }
@@ -52,9 +58,12 @@ extension Double : FloatingPointMath {
     public static func cosh (_ x:Double)->Double { return Darwin.cosh(x) }
     public static func exp  (_ x:Double)->Double { return Darwin.exp(x) }
     public static func log  (_ x:Double)->Double { return Darwin.log(x) }
+    public static func log10(_ x:Double)->Double { return Darwin.log10(x) }
     public static func sin  (_ x:Double)->Double { return Darwin.sin(x) }
     public static func sinh (_ x:Double)->Double { return Darwin.sinh(x) }
     public static func sqrt (_ x:Double)->Double { return Darwin.sqrt(x) }
+    public static func tan  (_ x:Double)->Double { return Darwin.tan(x) }
+    public static func tanh (_ x:Double)->Double { return Darwin.tanh(x) }
     public static func atan2(_ y:Double, _ x:Double)->Double { return Darwin.atan2(y, x) }
     public static func hypot(_ x:Double, _ y:Double)->Double { return Darwin.hypot(x, y) }
     public static func pow  (_ x:Double, _ y:Double)->Double { return Darwin.pow(x, y) }
@@ -73,9 +82,12 @@ extension Float : FloatingPointMath {
     public static func cosh (_ x:Float)->Float { return Glibc.cosh(x) }
     public static func exp  (_ x:Float)->Float { return Glibc.exp(x) }
     public static func log  (_ x:Float)->Float { return Glibc.log(x) }
+    public static func log10(_ x:Float)->Float { return Glibc.log10(x) }
     public static func sin  (_ x:Float)->Float { return Glibc.sin(x) }
     public static func sinh (_ x:Float)->Float { return Glibc.sinh(x) }
     public static func sqrt (_ x:Float)->Float { return Glibc.sqrt(x) }
+    public static func tan  (_ x:Float)->Float { return Glibc.tan(x) }
+    public static func tanh (_ x:Float)->Float { return Glibc.tanh(x) }
     public static func atan2(_ y:Float, _ x:Float)->Float { return Glibc.atan2(y, x) }
     public static func hypot(_ x:Float, _ y:Float)->Float { return Glibc.hypot(x, y) }
     public static func pow  (_ x:Float, _ y:Float)->Float { return Glibc.pow(x, y) }
@@ -90,9 +102,12 @@ extension Float : FloatingPointMath {
     public static func cosh (_ x:Float)->Float { return Darwin.cosh(x) }
     public static func exp  (_ x:Float)->Float { return Darwin.exp(x) }
     public static func log  (_ x:Float)->Float { return Darwin.log(x) }
+    public static func log10(_ x:Float)->Float { return Darwin.log10(x) }
     public static func sin  (_ x:Float)->Float { return Darwin.sin(x) }
     public static func sinh (_ x:Float)->Float { return Darwin.sinh(x) }
     public static func sqrt (_ x:Float)->Float { return Darwin.sqrt(x) }
+    public static func tan  (_ x:Float)->Float { return Darwin.tan(x) }
+    public static func tanh (_ x:Float)->Float { return Darwin.tanh(x) }
     public static func atan2(_ y:Float, _ x:Float)->Float { return Darwin.atan2(y, x) }
     public static func hypot(_ x:Float, _ y:Float)->Float { return Darwin.hypot(x, y) }
     public static func pow  (_ x:Float, _ y:Float)->Float { return Darwin.pow(x, y) }
